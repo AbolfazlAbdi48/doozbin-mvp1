37bfb066382b17045eee0ebcefb0e8a5f0175f03
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2024-08-23 21:30:29 +0330

    Update: account's admin register

--- account/admin.py
+++ account/admin.py
@@ -3,7 +3,38 @@ from django.contrib.auth.admin import UserAdmin
 
 from account.models import User, UserWallet, UserScan
 
+
 # Register your models here.
-admin.site.register(User, UserAdmin)
-admin.site.register(UserWallet)
-admin.site.register(UserScan)
+class UserWalletInline(admin.TabularInline):
+    model = UserWallet
+    extra = 0
+    show_change_link = True
+
+
+class UserScanInline(admin.TabularInline):
+    model = UserScan
+    extra = 0
+    show_change_link = True
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [
+        UserScanInline,
+        UserWalletInline
+    ]
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(UserWallet)
+class UserWalletAdmin(admin.ModelAdmin):
+    list_display = ['user', 'balance', 'created']
+    search_fields = ('user__username',)
+
+
+@admin.register(UserScan)
+class UserScanAdmin(admin.ModelAdmin):
+    list_display = ['user', 'coin', 'brand', 'accepted', 'created']
+    search_fields = ('brand__unique_name', 'brand__name', 'user__username')
+    list_filter = ('accepted',)
