00a6705112e48045bdfe31526ef3d7d78cc0b48c
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2024-08-31 23:30:31 +0330

    Update: settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if config('ON_SERVER') == 'True':
+if config('ON_SERVER') == 'False':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-elif config('ON_SERVER') == 'False':
+elif config('ON_SERVER') == 'True':
     DATABASES = {
         'default': {
             'ENGINE': config('DATABASE_ENGINE'),
