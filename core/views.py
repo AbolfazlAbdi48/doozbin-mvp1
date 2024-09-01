ea6ae1192f01ec495bdb473ce1272b708b259566
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2024-09-01 18:44:51 +0330

    Update: scan image

--- core/views.py
+++ core/views.py
@@ -24,52 +24,52 @@ def scan_page_view(request):
     return render(request, 'core/scan_page.html', context)
 
 
-@login_required
+# @login_required
 def scan_img_view(request):
     is_ajax = request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
-    if is_ajax:
-        if request.method == 'POST':
-            data = json.loads(request.body)
-            image_data = data['image']
-            image_data = image_data.split(',')[1]
-            image = Image.open(BytesIO(base64.b64decode(image_data)))
-            try:
-                wallet = UserWallet.objects.get(user=request.user)
-            except wallet.DoesNotExist:
-                wallet = UserWallet.objects.create(user=request.user, balance=0)
+    # if is_ajax:
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        image_data = data['image']
+        image_data = image_data.split(',')[1]
+        image = Image.open(BytesIO(base64.b64decode(image_data)))
+        try:
+            wallet = UserWallet.objects.get(user=request.user)
+        except wallet.DoesNotExist:
+            wallet = UserWallet.objects.create(user=request.user, balance=0)
 
-            image_io = BytesIO()
-            image.save(image_io, format='PNG')
-            image_file = ContentFile(image_io.getvalue(), 'brand.png')
+        image_io = BytesIO()
+        image.save(image_io, format='PNG')
+        image_file = ContentFile(image_io.getvalue(), 'brand.png')
 
-            scan = UserScan(
-                user=request.user,
-                coin=0,
-                description="first scan",
-                accepted=False,
-                scanned_img=image_file
-            )
-            scan.save()
+        scan = UserScan(
+            user=request.user,
+            coin=0,
+            description="first scan",
+            accepted=False,
+            scanned_img=image_file
+        )
+        scan.save()
 
-            results = model(scan.scanned_img.path)
-            detected = False
-            if results:
-                for result in results:
-                    if result.boxes:
-                        for box in result.boxes:
-                            if box.conf[0].item() >= 0.7:
-                                scan.description = "digikala"
-                                scan.coin = 25
-                                scan.accepted = True
-                                scan.save()
-                                wallet.balance += 25
-                                wallet.save()
-                                detected = True
-                                break
-                        if detected:
+        results = model(scan.scanned_img.path)
+        detected = False
+        if results:
+            for result in results:
+                if result.boxes:
+                    for box in result.boxes:
+                        if box.conf[0].item() >= 0.7:
+                            scan.description = "digikala"
+                            scan.coin = 25
+                            scan.accepted = True
+                            scan.save()
+                            wallet.balance += 25
+                            wallet.save()
+                            detected = True
                             break
+                    if detected:
+                        break
 
-            return JsonResponse({'status': detected})
-        return JsonResponse({'status': 'Invalid request'}, status=400)
-    return HttpResponseBadRequest('Invalid request')
+        return JsonResponse({'status': detected})
+    return JsonResponse({'status': 'Invalid request'}, status=400)
+    # return HttpResponseBadRequest('Invalid request')
