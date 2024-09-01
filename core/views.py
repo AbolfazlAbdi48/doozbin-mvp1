616c7044bb3e3e2edd45241ad039c4cbc5c7bb3f
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2024-09-01 16:03:42 +0330

    Update: complete scan_img_view

--- core/views.py
+++ core/views.py
@@ -6,10 +6,10 @@ from PIL import Image
 from django.contrib.auth.decorators import login_required
 from django.core.files.base import ContentFile
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
-from account.models import UserScan
+from account.models import UserScan, User, UserWallet
 from ultralytics import YOLO
 from django.conf import settings
 
@@ -24,45 +24,49 @@ def scan_page_view(request):
     return render(request, 'core/scan_page.html', context)
 
 
+@login_required
 def scan_img_view(request):
-    """
-    TODO: login required
-    TODO: check request is ajax
-    TODO: add coin to user wallet
-    TODO: connect ai model
-    """
-    data = json.loads(request.body)
-    image_data = data['image']
-    image_data = image_data.split(',')[1]
-    image = Image.open(BytesIO(base64.b64decode(image_data)))
+    is_ajax = request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
-    image_io = BytesIO()
-    image.save(image_io, format='PNG')
-    image_file = ContentFile(image_io.getvalue(), 'brand.png')
+    if is_ajax:
+        if request.method == 'POST':
+            data = json.loads(request.body)
+            image_data = data['image']
+            image_data = image_data.split(',')[1]
+            image = Image.open(BytesIO(base64.b64decode(image_data)))
+            wallet, created = UserWallet.objects.get_or_create(user=request.user, balance=0)
 
-    scan = UserScan(
-        user=request.user,
-        coin=0,
-        description="first scan",
-        accepted=False,
-        scanned_img=image_file
-    )
-    scan.save()
+            image_io = BytesIO()
+            image.save(image_io, format='PNG')
+            image_file = ContentFile(image_io.getvalue(), 'brand.png')
 
-    results = model(scan.scanned_img.path)
-    detected = False
-    if results:
-        for result in results:
-            if result.boxes:
-                for box in result.boxes:
-                    if box.conf[0].item() >= 0.7:
-                        scan.description = "digikala"
-                        scan.coin = 15
-                        scan.accepted = True
-                        scan.save()
-                        detected = True
-                        break
-                if detected:
-                    break
+            scan = UserScan(
+                user=request.user,
+                coin=0,
+                description="first scan",
+                accepted=False,
+                scanned_img=image_file
+            )
+            scan.save()
 
-    return JsonResponse({'status': 'OK'})
+            results = model(scan.scanned_img.path)
+            detected = False
+            if results:
+                for result in results:
+                    if result.boxes:
+                        for box in result.boxes:
+                            if box.conf[0].item() >= 0.7:
+                                scan.description = "digikala"
+                                scan.coin = 15
+                                scan.accepted = True
+                                scan.save()
+                                wallet.balance += 5
+                                wallet.save()
+                                detected = True
+                                break
+                        if detected:
+                            break
+
+            return JsonResponse({'status': 'OK'})
+        return JsonResponse({'status': 'Invalid request'}, status=400)
+    return HttpResponseBadRequest('Invalid request')
